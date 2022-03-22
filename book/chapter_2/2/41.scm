b1ef204d8d08e82056fb62ac9c92ad51fb46afd5
Tito Lins <titolins@outlook.com>
2022-03-22 07:33:29 +0100

    add 41

---
+++ book/chapter_2/2/41.scm
@@ -0,0 +1,30 @@
+;; helpers
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      '()
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (flatmap proc seq)
+  (accumulate append '() (map proc seq)))
+
+;; solution
+(define (unique-triples n)
+  (flatmap (lambda (i)
+             (flatmap (lambda (j)
+                        (map (lambda (k) (list i j k))
+                             (enumerate-interval 1 (- j 1))))
+                      (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(define (sum-s? s)
+  (lambda (triple)
+    (= (+ (car triple) (cadr triple) (caddr triple)) s)))
+
+(define (triples-with-sum n s)
+  (filter (sum-s? s) (unique-triples n)))
