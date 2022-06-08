885985f998956e4ce1294f2b11fa505344b27d70
Tito Lins <tito.lins@deliveryhero.com>
2022-06-08 06:54:57 +0200

    improve 2.54

--- book/chapter_2/2/54.scm
+++ book/chapter_2/2/54.scm
@@ -1,6 +1,14 @@
+;; first iteration
 (define (equal? a b)
   (cond ((and (null? a) (null? b)) #t)
         ((and (symbol? a) (symbol? b)) (eq? a b))
         ((and (number? a) (number? b)) (= a b))
         ((and (list? a) (list? b)) (and (equal? (car a) (car b)) (equal? (cdr a) (cdr b))))
+        (else #f)))
+
+;; we don't actually need to differentiate symbols, numbers, etc (can just use eq? for all not pairs)
+;; also, using pair? instead of list? seems like the most scheme-like option
+(define (equal? a b)
+  (cond ((and (not (pair? a)) (not (pair? b))) (eq? a b))
+        ((and (pair? a) (pair? b)) (and (equal? (car a) (car b)) (equal? (cdr a) (cdr b))))
         (else #f)))
